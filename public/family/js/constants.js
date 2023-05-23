3a1d3461dfd6a0cefbad9b22d40ecdc2617a8487
Arbyn Acosta <arbyn.acosta@dtn.com>
2023-05-23 14:04:24 +0800

    Update constants.js

--- public/family/js/constants.js
+++ public/family/js/constants.js
@@ -1,10 +1,6 @@
 const isPrivate = !(window.location.get("private") == "false");
 const isChecking = window.location.get("check") == "true";
 
-console.log(`Using dark theme: ${isDark}`);
-console.log(`Enabled private mode: ${isPrivate}`);
-console.log(`Enabled checking mode: ${isChecking}`);
-
 const ui = {
   scale: 0.85,
   font: {
