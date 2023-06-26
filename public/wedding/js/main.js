f7733aafe8a6e359889deb31c3aada66f7bc513d
Arbyn Acosta <arbyn.acosta@dtn.com>
2023-06-27 05:04:04 +0800

    Add working animation

---
+++ public/wedding/js/main.js
@@ -0,0 +1,30 @@
+// Create a new ScrollMagic controller
+var controller = new ScrollMagic.Controller();
+
+// Get all the target images
+var images = document.querySelectorAll(".engagement-ring .animation__images img");
+
+// Create a timeline
+var engagementRingTimeline = gsap.timeline();
+
+// Loop over each image
+for(let i = 0; i < images.length - 1; i++) {
+    let nextImage = images[i+1];
+    let currentImage = images[i];
+
+    engagementRingTimeline.addLabel(`frame${i}`)
+            .to(currentImage, 1, {className: "-=visible"}, `frame${i}`)
+            .to(currentImage, 1, {className: "+=hidden"}, `frame${i}`)
+            .to(nextImage, 1, {className: "-=hidden"}, `frame${i}`)
+            .to(nextImage, 1, {className: "+=visible"}, `frame${i}`);
+}
+
+// Create scene to pin and link animation
+new ScrollMagic.Scene({
+        triggerElement: ".engagement-ring.hero",
+        triggerHook: "onLeave",
+        duration: "300%"
+    })
+    .setPin(".engagement-ring.animation")
+    .setTween(engagementRingTimeline)
+    .addTo(controller);
