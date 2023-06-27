81a51acf23e3bd33885686dadc50dc6df99b5a03
Arbyn Acosta <arbyn.acosta@dtn.com>
2023-06-28 05:20:04 +0800

    Add text and navbar

--- public/wedding/js/main.js
+++ public/wedding/js/main.js
@@ -9,22 +9,22 @@ var engagementRingTimeline = gsap.timeline();
 
 // Loop over each image
 for(let i = 0; i < images.length - 1; i++) {
-    let nextImage = images[i+1];
-    let currentImage = images[i];
+  let nextImage = images[i+1];
+  let currentImage = images[i];
 
-    engagementRingTimeline.addLabel(`frame${i}`)
-            .to(currentImage, 1, {className: "-=visible"}, `frame${i}`)
-            .to(currentImage, 1, {className: "+=hidden"}, `frame${i}`)
-            .to(nextImage, 1, {className: "-=hidden"}, `frame${i}`)
-            .to(nextImage, 1, {className: "+=visible"}, `frame${i}`);
+  engagementRingTimeline.addLabel(`frame${i}`)
+    .to(currentImage, 1, {className: "-=visible"}, `frame${i}`)
+    .to(currentImage, 1, {className: "+=hidden"}, `frame${i}`)
+    .to(nextImage, 1, {className: "-=hidden"}, `frame${i}`)
+    .to(nextImage, 1, {className: "+=visible"}, `frame${i}`);
 }
 
 // Create scene to pin and link animation
 new ScrollMagic.Scene({
-        triggerElement: ".engagement-ring.hero",
-        triggerHook: "onLeave",
-        duration: "200%"
-    })
-    .setPin(".engagement-ring.animation")
-    .setTween(engagementRingTimeline)
-    .addTo(controller);
+    triggerElement: ".engagement-ring.hero",
+    triggerHook: "onLeave",
+    duration: "200%"
+  })
+  .setPin(".engagement-ring.animation")
+  .setTween(engagementRingTimeline)
+  .addTo(controller);
