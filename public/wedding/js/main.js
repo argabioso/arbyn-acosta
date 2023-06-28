e789a0b434b59c3873b60fbeeac1504ba59623df
Arbyn Acosta <arbyn.acosta@dtn.com>
2023-06-29 02:00:58 +0800

    Increase ring quality

--- public/wedding/js/main.js
+++ public/wedding/js/main.js
@@ -1,30 +1,32 @@
-// Create a new ScrollMagic controller
-var controller = new ScrollMagic.Controller();
+window.addEventListener('load', function() {
+  // Create a new ScrollMagic controller
+  var controller = new ScrollMagic.Controller();
 
-// Get all the target images
-var images = document.querySelectorAll(".engagement-ring .animation__images img");
+  // Get all the target images
+  var images = document.querySelectorAll(".engagement-ring .animation__images img");
 
-// Create a timeline
-var engagementRingTimeline = gsap.timeline();
+  // Create a timeline
+  var engagementRingTimeline = gsap.timeline();
 
-// Loop over each image
-for(let i = 0; i < images.length - 1; i++) {
-  let nextImage = images[i+1];
-  let currentImage = images[i];
+  // Loop over each image
+  for(let i = 0; i < images.length - 1; i++) {
+    let nextImage = images[i+1];
+    let currentImage = images[i];
 
-  engagementRingTimeline.addLabel(`frame${i}`)
-    .to(currentImage, 1, {className: "-=visible"}, `frame${i}`)
-    .to(currentImage, 1, {className: "+=hidden"}, `frame${i}`)
-    .to(nextImage, 1, {className: "-=hidden"}, `frame${i}`)
-    .to(nextImage, 1, {className: "+=visible"}, `frame${i}`);
-}
+    engagementRingTimeline.addLabel(`frame-${i}`)
+      .to(currentImage, 1, {className: "-=visible"}, `frame-${i}`)
+      .to(currentImage, 1, {className: "+=hidden"}, `frame-${i}`)
+      .to(nextImage, 1, {className: "-=hidden"}, `frame-${i}`)
+      .to(nextImage, 1, {className: "+=visible"}, `frame-${i}`);
+  }
 
-// Create scene to pin and link animation
-new ScrollMagic.Scene({
-    triggerElement: ".engagement-ring.hero",
-    triggerHook: "onLeave",
-    duration: "200%"
-  })
-  .setPin(".engagement-ring.hero")
-  .setTween(engagementRingTimeline)
-  .addTo(controller);
+  // Create scene to pin and link animation
+  new ScrollMagic.Scene({
+      triggerElement: ".engagement-ring.hero",
+      triggerHook: "onLeave",
+      duration: "200%"
+    })
+    .setPin(".engagement-ring.hero")
+    .setTween(engagementRingTimeline)
+    .addTo(controller);
+});
