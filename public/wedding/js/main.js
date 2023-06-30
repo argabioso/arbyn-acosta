fa6fd8d32e92ca1310c143110eaf1ba2042c0953
Arbyn Acosta <arbyn.acosta@dtn.com>
2023-06-30 23:55:14 +0800

    Dynamic title thickness

--- public/wedding/js/main.js
+++ public/wedding/js/main.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function() {
   var controller = new ScrollMagic.Controller();
 
   // Get all the target images
-  var images = document.querySelectorAll(".engagement-ring .animation__images img");
+  var images = document.querySelectorAll('.engagement-ring .animation__images img');
 
   // Create a timeline
   var engagementRingTimeline = gsap.timeline();
@@ -14,19 +14,34 @@ window.addEventListener('load', function() {
     let currentImage = images[i];
 
     engagementRingTimeline.addLabel(`frame-${i}`)
-      .to(currentImage, 1, {className: "-=visible"}, `frame-${i}`)
-      .to(currentImage, 1, {className: "+=hidden"}, `frame-${i}`)
-      .to(nextImage, 1, {className: "-=hidden"}, `frame-${i}`)
-      .to(nextImage, 1, {className: "+=visible"}, `frame-${i}`);
+      .to(currentImage, 1, {className: '-=visible'}, `frame-${i}`)
+      .to(currentImage, 1, {className: '+=hidden'}, `frame-${i}`)
+      .to(nextImage, 1, {className: '-=hidden'}, `frame-${i}`)
+      .to(nextImage, 1, {className: '+=visible'}, `frame-${i}`);
   }
 
   // Create scene to pin and link animation
   new ScrollMagic.Scene({
-      triggerElement: ".engagement-ring.hero",
-      triggerHook: "onLeave",
-      duration: "200%"
+      triggerElement: '.engagement-ring.hero',
+      triggerHook: 'onLeave',
+      duration: '200%'
     })
-    .setPin(".engagement-ring.hero")
+    .setPin('.engagement-ring.hero')
     .setTween(engagementRingTimeline)
     .addTo(controller);
+
+  // Select the image
+  let image = document.querySelector('#title');
+
+  // Define the function that changes the image source
+  function changeImageSrc() {
+    if (window.innerWidth > 600) {
+      image.src = 'images/title.svg';
+    } else {
+      image.src = 'images/title.webp';
+    }
+  }
+
+  changeImageSrc();
+  window.addEventListener('resize', changeImageSrc);
 });
