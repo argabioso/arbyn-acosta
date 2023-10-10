2b6b5ee6249a50a81e949c9b4486f476001dacc9
arbyn-argabioso <arbyn.acosta@gmail.com>
2023-10-11 02:27:47 +0800

    Combine JS files

---
+++ public/family/scripts/combine.sh
@@ -0,0 +1,14 @@
+filename="js/family.js"
+echo "" > $filename
+cat js/constants.js >> $filename
+cat js/chart.js >> $filename
+cat js/data.js >> $filename
+cat js/sources.js >> $filename
+cat js/checks.js >> $filename
+cat js/template/utils.js >> $filename
+cat js/template/node.js >> $filename
+cat js/template/link.js >> $filename
+cat js/template/photo.js >> $filename
+cat js/template/markers.js >> $filename
+cat js/template/text.js >> $filename
+cat js/main.js >> $filename
