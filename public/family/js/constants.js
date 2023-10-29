b7c0c793d1767215aac853ae481f72202a25277e
arbyn-argabioso <arbyn.acosta@gmail.com>
2023-10-29 16:06:12 +0800

    Remove Unused layouts and models

--- public/family/js/constants.js
+++ public/family/js/constants.js
@@ -2,7 +2,7 @@ const isPrivate = !(window.location.get("private") == "false");
 var isChecking = false; // overridden by checks.js if imported
 
 const ui = {
-  scale: 1,
+  scale: 0.85,
   font: {
     size: {
       name: 15,
