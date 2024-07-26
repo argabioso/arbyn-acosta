92bff57e079d57d80ec5735103f16750bfe28c4c
arbyn-argabioso <arbyn@argabioso.com>
2024-07-27 01:45:43 +0800

    Add sidebar to combine.sh

--- public/family/scripts/combine.sh
+++ public/family/scripts/combine.sh
@@ -11,6 +11,7 @@ cat js/data.js >> $filename
 cat js/preprocess.js >> $filename
 cat js/sources.js >> $filename
 cat js/checks.js >> $filename
+cat js/sidebar.js >> $filename
 cat js/template/utils.js >> $filename
 cat js/template/node.js >> $filename
 cat js/template/link.js >> $filename
